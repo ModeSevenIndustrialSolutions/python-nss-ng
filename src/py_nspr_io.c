f06b2984897c36e57e8e4c2ac9d8452b3cd74078
Matthew Watkins <mwatkins@linuxfoundation.org>
2025-11-17 09:53:23 +0000

    Fix: Issues with the C code, broken links in README.md
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- src/py_nspr_io.c
+++ src/py_nspr_io.c
@@ -1,8 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * SPDX-License-Identifier: MPL-2.0
  * SPDX-FileCopyrightText: Copyright (c) 2010-2025 python-nss contributors
+ */
 
 // FIXME: add detail to each set_nspr_error()
 // FIXME: should nspr exception be derived from IOError? Note our detail is IOError's filename
